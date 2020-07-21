ff3fc23199669b042f75ada0cac09afc067904a5
BuildTools <unconfigured@null.spigotmc.org>
2020-07-21 13:17:30 +0200

    Added missing 'element.' .

--- main.js
+++ main.js
@@ -9,16 +9,16 @@ var muted_users     = [ { username: 'test#0000', timeout: 3 } ];
 
 function lower_risk_level() {
   warning_1_users.forEach(element => {
-    if (timeout > 0)
-    timeout--;
+    if (element.timeout > 0)
+    element.timeout--;
   });
   warning_2_users.forEach(element => {
-    if (timeout > 0)
-    timeout--;
+    if (element.timeout > 0)
+    element.timeout--;
   });
   muted_users.forEach(element => {
-    if (timeout > 0)
-    timeout--;
+    if (element.timeout > 0)
+    element.timeout--;
   });
   console.log('Lowered warning levels!');
 }
@@ -36,6 +36,7 @@ client.on('message', msg => {
   for (let i = 0; i < censored_words.length; i++) {
     if (msg.content.toLowerCase().includes(censored_words[i])) {
       needs_censor = true;
+      console.log('Need censor!');
     }
   }
 
@@ -44,18 +45,21 @@ client.on('message', msg => {
       if (msg.author.toString() == element.username && element.timeout > 0) {
         censor_level = 1;
         element.timeout = 3;
+        console.log('Found in w1!');
       }
     });
     warning_2_users.forEach(element => {
       if (msg.author.toString() == element.username && element.timeout > 0) {
         censor_level = 2;
         element.timeout = 3;
+        console.log('Found in w2!');
       }
     });
     muted_users.forEach(element => {
       if (msg.author.toString() == element.username && element.timeout > 0) {
         censor_level = 3;
         element.timeout = 3;
+        console.log('Found in mt!');
       }
     });
 
@@ -63,16 +67,19 @@ client.on('message', msg => {
       case 0: {
         warning_1_users.push({ username: msg.author, timeout: 3 }); 
         msg.reply(process.env.RESPONSE_MSG_1); 
+        console.log('Added to w1!');
         break;
       }
       case 1: {
         warning_2_users.push({ username: msg.author, timeout: 3 }); 
         msg.reply(process.env.RESPONSE_MSG_2); 
+        console.log('Added to w2!');
         break;
       }
       case 2: {
         muted_users.push    ({ username: msg.author, timeout: 3 }); 
         msg.reply(process.env.RESPONSE_MSG_3); 
+        console.log('Added to mt!');
         break;
       }
     }

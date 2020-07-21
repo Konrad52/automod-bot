c2f5c1beb1033d59d99c7e2fa4d5085f518a7586
BuildTools <unconfigured@null.spigotmc.org>
2020-07-21 22:06:41 +0200

    Added webpage.

--- page.js
+++ page.js
@@ -1,7 +1,23 @@
-var http = require('http');
+const http = require('http');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello World!');
-  res.end();
+http.createServer((request, response) => {
+  const { headers, method, url } = request;
+  let body = [];
+  request.on('error', (err) => {
+    console.error(err);
+  }).on('data', (chunk) => {
+    body.push(chunk);
+  }).on('end', () => {
+    body = Buffer.concat(body).toString();
+
+    response.on('error', (err) => {
+      console.error(err);
+    });
+
+    response.writeHead(200, {'Content-Type': 'application/json'})
+
+    const responseBody = { headers, method, url, body };
+    
+    response.end(JSON.stringify(responseBody))
+  });
 }).listen(8080);

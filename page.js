4a5693d11f38fd9d96bd8d8f5c8ec5ce419972fe
BuildTools <unconfigured@null.spigotmc.org>
2020-07-21 22:27:16 +0200

    Added index.html

--- page.js
+++ page.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('hello world');
+    res.sendFile('pages/intex.html', {root: __dirname })
 });
 
 app.listen(process.env.PORT);

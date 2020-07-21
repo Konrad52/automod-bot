f10062ef4354f6fb000abbc261d0f2efbac3b912
BuildTools <unconfigured@null.spigotmc.org>
2020-07-21 13:00:23 +0200

    Added timeout system.

--- main.js
+++ main.js
@@ -3,21 +3,74 @@ const client = new Discord.Client();
 
 const censored_words = process.env.CENSORED_WORDS.split(',');
 
+var warning_1_users = [ { username: 'test#0000', timeout: 3 } ];
+var warning_2_users = [ { username: 'test#0000', timeout: 3 } ];
+var muted_users     = [ { username: 'test#0000', timeout: 3 } ];
+
+async function lower_risk_level() {
+  warning_1_users.forEach(element => {
+    if (timeout > 0)
+    timeout--;
+  });
+  warning_2_users.forEach(element => {
+    if (timeout > 0)
+    timeout--;
+  });
+  muted_users.forEach(element => {
+    if (timeout > 0)
+    timeout--;
+  });
+}
+
+setInterval(lower_risk_level, 60000);
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
   var needs_censor = false;
+  var censor_level = 0;
 
   for (let i = 0; i < censored_words.length; i++) {
-    if (msg.content.toLowerCase().includes(censored_words[i])) 
+    if (msg.content.toLowerCase().includes(censored_words[i])) {
       needs_censor = true;
+
+      warning_1_users.forEach(element1 => {
+        if (msg.author.toString() == element1.username && element1.timeout > 0) {
+          censor_level = 1;
+          element1.timeout = 3;
+        }
+      });
+      warning_2_users.forEach(element2 => {
+        if (msg.author.toString() == element2.username && element2.timeout > 0) {
+          censor_level = 2;
+          element2.timeout = 3;
+        }
+      });
+      muted_users.forEach(element3 => {
+        if (msg.author.toString() == element3.username && element3.timeout > 0) {
+          censor_level = 3;
+          element3.timeout = 3;
+        }
+      });
+
+      switch (censor_level) {
+        case 0: warning_1_users.push({ username: msg.author, timeout: 3 });
+        case 1: warning_2_users.push({ username: msg.author, timeout: 3 });
+        case 2: muted_users.push    ({ username: msg.author, timeout: 3 });
+      }
+    }
   }
 
   if (needs_censor) {
     msg.delete();
-    msg.reply(process.env.RESPONSE_MSG);
+    
+    switch (censor_level) {
+      case 0: msg.reply(process.env.RESPONSE_MSG_1);
+      case 1: msg.reply(process.env.RESPONSE_MSG_2);
+      case 2: msg.reply(process.env.RESPONSE_MSG_3);
+    }
   }
 });
 

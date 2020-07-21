5f2ad3be55c36a48f688015a8f7430b3b493bc5a
BuildTools <unconfigured@null.spigotmc.org>
2020-07-21 22:10:15 +0200

    Added webpage.

--- page.js
+++ page.js
@@ -1,23 +1,8 @@
-const http = require('http');
+var express = require('express');
+var app = express();
 
-http.createServer((request, response) => {
-  const { headers, method, url } = request;
-  let body = [];
-  request.on('error', (err) => {
-    console.error(err);
-  }).on('data', (chunk) => {
-    body.push(chunk);
-  }).on('end', () => {
-    body = Buffer.concat(body).toString();
+app.get('/', function(req, res){
+  res.send('hello world');
+});
 
-    response.on('error', (err) => {
-      console.error(err);
-    });
-
-    response.writeHead(200, {'Content-Type': 'application/json'})
-
-    const responseBody = { headers, method, url, body };
-    
-    response.end(JSON.stringify(responseBody))
-  });
-}).listen(8080);
+app.listen(3000);

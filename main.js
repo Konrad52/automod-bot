00324affb312152514d3c08cc752be331af8a022
BuildTools <unconfigured@null.spigotmc.org>
2020-07-21 13:29:09 +0200

    Made improvements.

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const client = new Discord.Client();
 
 const censored_words = process.env.CENSORED_WORDS.split(',');
 
-var warning_1_users = [ { username: 'test#0000', timeout: 3 } ];
-var warning_2_users = [ { username: 'test#0000', timeout: 3 } ];
-var muted_users     = [ { username: 'test#0000', timeout: 3 } ];
+var warning_1_users = [ { username: '', timeout: 3 } ];
+var warning_2_users = [ { username: '', timeout: 3 } ];
+var muted_users     = [ { username: '', timeout: 3 } ];
 
 function lower_risk_level() {
   warning_1_users.forEach(element => {
@@ -20,7 +20,6 @@ function lower_risk_level() {
     if (element.timeout > 0)
     element.timeout--;
   });
-  console.log('Lowered warning levels!');
 }
 
 setInterval(lower_risk_level, 60000);
@@ -32,56 +31,68 @@ client.on('ready', () => {
 client.on('message', msg => {
   var needs_censor = false;
   var censor_level = 0;
+  var array_level  = 0;
 
   for (let i = 0; i < censored_words.length; i++) {
     if (msg.content.toLowerCase().includes(censored_words[i])) {
       needs_censor = true;
-      console.log('Need censor!');
     }
   }
 
+  muted_users.forEach(element => {
+    if (msg.author.toString() == element.username && element.timeout > 0) {
+      needs_censor = true;
+    }
+  });
+
   if (needs_censor) {
     warning_1_users.forEach(element => {
-      if (msg.author.toString() == element.username && element.timeout > 0) {
-        censor_level = 1;
-        element.timeout = 3;
-        console.log('Found in w1!');
+      if (msg.author.toString() == element.username) {
+        if (element.timeout > 0) {
+          censor_level = 1;
+          element.timeout = 3;
+        }
+        array_level = 1;
       }
     });
     warning_2_users.forEach(element => {
-      if (msg.author.toString() == element.username && element.timeout > 0) {
-        censor_level = 2;
-        element.timeout = 3;
-        console.log('Found in w2!');
+      if (msg.author.toString() == element.username) {
+        if (element.timeout > 0) {
+          censor_level = 2;
+          element.timeout = 3;
+        }
+        array_level = 2;
       }
     });
     muted_users.forEach(element => {
-      if (msg.author.toString() == element.username && element.timeout > 0) {
-        censor_level = 3;
-        element.timeout = 3;
-        console.log('Found in mt!');
+      if (msg.author.toString() == element.username) {
+        if (element.timeout > 0) {
+          censor_level = 3;
+          element.timeout = 3;
+        }
+        array_level = 3;
       }
     });
 
     switch (censor_level) {
       case 0: {
-        warning_1_users.push({ username: msg.author.toString(), timeout: 3 }); 
+        if (array_level < 1)
+          warning_1_users.push({ username: msg.author.toString(), timeout: 3 }); 
         msg.reply(process.env.RESPONSE_MSG_1); 
-        console.log('Added to w1!');
         console.log(warning_1_users.toString());
         break;
       }
       case 1: {
-        warning_2_users.push({ username: msg.author.toString(), timeout: 3 }); 
+        if (array_level < 2)
+          warning_2_users.push({ username: msg.author.toString(), timeout: 3 }); 
         msg.reply(process.env.RESPONSE_MSG_2); 
-        console.log('Added to w2!');
         console.log(warning_2_users.toString());
         break;
       }
       case 2: {
-        muted_users.push    ({ username: msg.author.toString(), timeout: 3 }); 
+        if (array_level < 3)
+          muted_users.push({ username: msg.author.toString(), timeout: 3 }); 
         msg.reply(process.env.RESPONSE_MSG_3); 
-        console.log('Added to mt!');
         console.log(muted_users.toString());
         break;
       }

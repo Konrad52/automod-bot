90c7ab88c6056c2d476b7dbd746e0fe92a9a671a
BuildTools <unconfigured@null.spigotmc.org>
2020-07-21 22:32:10 +0200

    Added index.html

--- page.js
+++ page.js
@@ -2,7 +2,19 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    res.sendFile('pages/intex.html', {root: __dirname })
+  res.send(
+    `<!DOCTYPE html>
+    <html lang="hu">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Automoderátor</title>
+    </head>
+    <body>
+        <button href="https://discord.com/api/oauth2/authorize?client_id=735059927688675339&permissions=470080&scope=bot">Meghívás a szerverre...</button>
+    </body>
+    </html>`
+  );
 });
 
 app.listen(process.env.PORT);

e300710a3516889600d163322399c3e4d3dd8fb5
BuildTools <unconfigured@null.spigotmc.org>
2020-07-21 11:37:33 +0200

    Improved the automoderation.

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const censored_words = [ 'fasz', 'pina', 'kurva', 'picsa', 'geci', 'köcsög', 'nyomorék' ]
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content.includes('fasz')) {
-    msg.reply('Enyje, nem szabad így beszélni.');
+  var needs_censor = false;
+
+  for (let i = 0; i < censored_words.length; i++) {
+    if (msg.content.includes(censored_words[i])) 
+      needs_censor = true;
+  }
+
+  if (needs_censor) {
+    message.delete(1000);
+    msg.reply('Tiltott szavakat használtál az üzenetetben! A megfelelő szóhasználatra kérlek figyelj oda.');
   }
 });
 

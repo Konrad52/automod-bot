a03074b190ac305381e7e8ff3c915fa48b298444
Konrád Kiss <54140292+Konrad52@users.noreply.github.com>
2020-07-21 11:09:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("lul");
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+client.login('NzM1MDU5OTI3Njg4Njc1MzM5.Xxav-A.59gG_esvvgU4AL1caiGK_J9NMK8');

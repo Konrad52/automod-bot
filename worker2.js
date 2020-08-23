782c3c19c1efb1d9348696503ac95f5dbacf1ee1
BuildTools <unconfigured@null.spigotmc.org>
2020-08-23 15:07:57 +0200

    Added a test.

--- worker2.js
+++ worker2.js
@@ -5,4 +5,18 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('messageReactionAdd', async (reaction, user) => {
+    if (reaction.partial) {
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			console.log('Something went wrong when fetching the message: ', error);
+			return;
+		}
+    }
+    
+    if (reaction.message.content.contains('Teszt'))
+        reaction.message.react(client.emojis.get("745207133657890856"))
+});
+
 client.login(process.env.TOKEN2);

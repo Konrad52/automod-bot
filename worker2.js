6116ae0bae00ea41c551b944b46ec335a5873e45
BuildTools <unconfigured@null.spigotmc.org>
2020-08-23 15:26:24 +0200

    Added a message

--- worker2.js
+++ worker2.js
@@ -5,6 +5,40 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('message', msg => {
+    if (msg.member == null)
+        return;
+
+    if (msg.content.startsWith('!')) {
+        if (msg.content.startsWith('!shophere')) {
+            const embed = new Discord.MessageEmbed()
+                .setColor('#ffaa00')
+                .setTitle('Gladiátor Leltár')
+                .setDescription(`Üdvözöllek!\n\n
+                                Ez itt a szerverünk leltára, ahonnan tárgyakat vehetsz ki\n
+                                magadnak égkövekért cserébe.\n\n
+                                Hogy honnan szerezhetsz égkövet?\n
+                                Nos, pár feltétel után kapsz egyet-egyet a játékaid után:\n
+                                 1. - Egy mérkőzés során elérted a 15 hősgyilkosságot.\n
+                                 2. - <>\n
+                                 3. - <>\n
+                                \nHonnan tudom hogy mennyi érmém van?\n
+                                 - Reagálj ezzel az emotikonnal <>, és elmondom neked.\n 
+                                \nMiféle tárgyink vannak?\n
+                                <baltaEmoji> - Ezzel egy kibannolt hőst hozhatsz a következő mérkőzéseden.\n
+                                <> - <>\n
+                                <> - <>\n
+                                \n! Minden tárgy csak a vétel napján felhasználható !\n
+                                Bármelyiknek a megvételéhez csak nyomd meg a megfelelő emotikont.\n
+                                \nSok szerencsét kívánok a tárgyak megszerzéséhez és mindekinek jó játékot!`)
+                .setTimestamp()
+                .setFooter('- Árus', 'https://cdn.discordapp.com/app-icons/747074877936369747/b581530bac729aea32d2fd655deee5ef.png');
+
+            msg.channel.send(embed);
+        }
+    }
+});
+
 client.on('messageReactionAdd', async (reaction, user) => {
     if (reaction.partial) {
 		try {

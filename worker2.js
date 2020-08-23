dad282c2a3cd1b6b013381ac9846af198f219ba3
BuildTools <unconfigured@null.spigotmc.org>
2020-08-23 16:56:00 +0200

    Updated text.

--- worker2.js
+++ worker2.js
@@ -15,28 +15,28 @@ client.on('message', msg => {
                 .setColor('#ffaa00')
                 .setTitle('Gladiátor Leltár')
                 .setDescription(`**   Üdvözöllek!**\n
-                                Ez itt a szerverünk leltára, ahonnan jutalmakat válthatsz ki magadnak égkőért cserébe.\n
-                                **Hogy honnan szerezhetsz égkövet?**
-                                Nos, pár feltétel után kapsz egyet a játékaid után:
+                                Ez itt a szerverünk leltára, ahonnan jutalmakat válthatsz ki magadnak drágakőért cserébe.\n
+                                **Hogy honnan szerezhetsz drágakövet?**
+                                Nos, pár feltétel után kapsz egyet a játékaid során:
                                 > 1. Elérted a <@&720757882488094730> rangot.
                                 > 2. Egy mérkőzés során elérted a 15 hősgyilkosságot.
                                 > 3. <>
-                                \n**Honnan tudom hogy van-e éppen égkövem?**
-                                 - Reagálj ezzel az emotikonnal <:egko:747103628619415615> és egy privát üzenetben elküldöm.
+                                \n**Honnan tudom hogy van-e éppen drágakövem?**
+                                 - Reagálj ezzel az emotikonnal <:gemstone:747106274885238935> és egy privát üzenetben elküldöm.
                                 \n**Miféle jutalmaink vannak?**
                                 <:glad_banned:747103629810466896> - Ezzel egy kibannolt hőst hozhatsz a következő mérkőzéseden.
                                 <:gladi_reuse:747103629860929617> - Ezzel még **EGY** alkalommal játszahatod aznap az egyik hősödet.
                                 <:gladi_item3:747103629504151673> - Valami \`:P\`\n
                                 > ❗ __**Minden tárgy csak a vétel napján felhasználható**__
-                                > ❗ __**Egyszerre csak egy égkő lehet egy felhasználónál.**__
-                                \nBármelyik tágynak a megvételéhez csak nyomd meg a megfelelő emotikont.
-                                \nSok szerencsét kívánok a tárgyak megszerzéséhez és mindekinek jó játékot!`)
+                                > ❗ __**Egyszerre csak egy drágakő lehet egy felhasználónál.**__
+                                \nBármelyik jutalom a kiváltásához csak nyomd meg a megfelelő emotikont.
+                                \nSok szerencsét kívánok a jutalmakat megszerzéséhez és mindekinek jó játékot!`)
                 .setTimestamp()
-                .setFooter('- Árus', 'https://cdn.discordapp.com/app-icons/747074877936369747/b581530bac729aea32d2fd655deee5ef.png');
+                .setFooter('- Árus');
 
             msg.channel.send(embed);
 
-            msg.react('747103628619415615').then(() =>
+            msg.react('747106274885238935').then(() =>
             msg.react('747103629810466896').then(() =>
             msg.react('747103629860929617').then(() =>
             msg.react('747103629504151673'))));
